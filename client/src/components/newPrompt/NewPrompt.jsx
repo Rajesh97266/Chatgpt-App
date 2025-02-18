e2193a8d1959f42a301cd04455aefa3c31d7a68a
Rajesh97266 <rajesh97266@gmail.com>
2025-02-18 18:04:26 +0530

    gemini-ai-integrated

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -1,23 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./newPrompt.css";
 import { useRef, useEffect } from "react";
+import Upload from "../upload/Upload";
+import { IKImage } from "imagekitio-react";
+import model from "../../lib/gemini";
+import Markdown from "react-markdown";
 
 const NewPrompt = () => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [img, setImage] = useState({
+    isLoading: false,
+    error: "",
+    dbData: {},
+  });
+
   const endRef = useRef(null);
 
   useEffect(() => {
     endRef.current?.scrollIntoView({ behavior: "smooth" });
   }, []);
 
+  const add = async (text) => {
+    setQuestion(text);
+    const result = await model.generateContent(text);
+    const response = await result.response;
+    setAnswer(response.text());
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const text = e.target.text.value;
+    if (!text) return;
+    add(text);
+  };
+
   return (
     <>
+      {img.isLoading && <div>Loading...</div>}
+      {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width="380"
+          transformation={[{ width: 380 }]}
+        />
+      )}
+      {question && <div className="message user">{question}</div>}
+      {answer && (
+        <div className="message">
+          <Markdown>{answer}</Markdown>
+        </div>
+      )}
       <div className="endChat" ref={endRef}></div>
-      <form className="newForm">
-        <label htmlFor="file">
-          <img src="/attachment.png" alt="" />
-        </label>
+      <form className="newForm" onSubmit={handleSubmit}>
+        <Upload setImage={setImage} />
         <input type="file" multiple={false} id="file" hidden />
-        <input type="text" placeholder="Ask Anything..." />
+        <input type="text" name="text" placeholder="Ask Anything..." />
         <button>
           <img src="/arrow.png" alt="" />
         </button>
